0b10f8eced1f397a49eea2126c4ee9b84b83d62f
esxr <dhoolia.pranav@gmail.com>
2024-06-07 12:59:19 +1000

    bug fix: shapes don't go out of boundary

--- ball_bounce/shape.py
+++ ball_bounce/shape.py
@@ -22,35 +22,37 @@ class Shape:
 
     def bounce(self, boundary):
         collision = False
+        max_size = min(boundary.width, boundary.height) / 2 - 5
+
         if self.position[0] - self.size <= boundary.x:
             self.speed[0] = abs(self.speed[0]) * self.speed_increase_factor
             self.position[0] = boundary.x + self.size
-            self.change_properties()
+            self.change_properties(max_size)
             collision = True
         elif self.position[0] + self.size >= boundary.x + boundary.width:
             self.speed[0] = -abs(self.speed[0]) * self.speed_increase_factor
             self.position[0] = boundary.x + boundary.width - self.size
-            self.change_properties()
+            self.change_properties(max_size)
             collision = True
         
         if self.position[1] - self.size <= boundary.y:
             self.speed[1] = abs(self.speed[1]) * self.speed_increase_factor
             self.position[1] = boundary.y + self.size
-            self.change_properties()
+            self.change_properties(max_size)
             collision = True
         elif self.position[1] + self.size >= boundary.y + boundary.height:
             self.speed[1] = -abs(self.speed[1]) * self.speed_increase_factor
             self.position[1] = boundary.y + boundary.height - self.size
-            self.change_properties()
+            self.change_properties(max_size)
             collision = True
 
         return collision
 
-    def change_properties(self):
+    def change_properties(self, max_size):
         if self.color_change:
             self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
         if self.size_change != 0:
             self.size += self.growth_rate * self.size * (1 - self.size / self.carrying_capacity)
-        self.size = max(10, self.size)
+        self.size = min(max(10, self.size), max_size)
         if self.sides is not None:
             self.sides = min(12, self.sides + self.sides_change)
